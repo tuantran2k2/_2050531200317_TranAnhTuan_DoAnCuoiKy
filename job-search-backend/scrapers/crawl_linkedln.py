8010e8718a93b4c4933b65a4029f0d9e30010dc8
tuantran2k2 <khnh113a@gmail.com>
2024-11-03 21:50:07 +0700

    update_jobs

--- job-search-backend/scrapers/crawl_linkedln.py
+++ job-search-backend/scrapers/crawl_linkedln.py
@@ -36,10 +36,6 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logging.info(f"Đang lọc lại tài liệu trên qdrant")
 
 collections = _qdrant.qdrant_client.get_collections().collections
-
-if not any(col.name == COLLECTION_NAME for col in collections):
-    print("ssss")
-    _qdrant.create_vector_db(COLLECTION_NAME)
     
 try:
     _qdrant.delete_old_points(COLLECTION_NAME)
@@ -143,57 +139,57 @@ for page_num in range(1, 20):
     # Trích xuất thông tin từ mỗi công việc
     for job in job_postings:
         try:
-            job_id = job.get('data-job-id', None)
-            existing_job, _ = _qdrant.qdrant_client.scroll(
-                collection_name=COLLECTION_NAME,
-                scroll_filter=models.Filter(
-                    must=[models.FieldCondition(key="id_job", match=models.MatchValue(value=job_id))]
-                ),
-                limit=1
-            )
-            if existing_job:
-                logging.info(f"Job với id_job {job_id} đã tồn tại trong Qdrant.")
+            # job_id = job.get('data-job-id', None)
+            # existing_job, _ = _qdrant.qdrant_client.scroll(
+            #     collection_name=COLLECTION_NAME,
+            #     scroll_filter=models.Filter(
+            #         must=[models.FieldCondition(key="id_job", match=models.MatchValue(value=job_id))]
+            #     ),
+            #     limit=1
+            # )
+            # if existing_job:
+            #     logging.info(f"Job với id_job {job_id} đã tồn tại trong Qdrant.")
+            #     continue
+        
+            job_link = f"https://www.linkedin.com/jobs/view/{job_id}"
+            logging.info(f"Đang truy cập chi tiết công việc: {job_link}")
+            driver.get(job_link)
+            time.sleep(5)
+            job_soup = BeautifulSoup(driver.page_source, "html.parser")
+            try:
+                job_title = job_soup.find("h1", {"class": "t-24 t-bold inline"}).text.strip()
+                job_card = job_soup.find('div', class_='job-details-jobs-unified-top-card__primary-description-container')
+                text_content = job_card.get_text(separator=' ', strip=True)
+                parts = text_content.split(' · ')
+                location = parts[0]
+                time_posted = parts[1]
+                exact_time = convert_to_datetime(time_posted)
+                date_format = convert_to_str(exact_time) if exact_time else None
+                logging.info(f"Title: {job_title}, Location: {location}, Date: {date_format}")
+            except AttributeError as e:
+                logging.error(f"Lỗi khi lấy thông tin chi tiết công việc: {e}")
                 continue
-            else:
-                job_link = f"https://www.linkedin.com/jobs/view/{job_id}"
-                logging.info(f"Đang truy cập chi tiết công việc: {job_link}")
-                driver.get(job_link)
-                time.sleep(5)
-                job_soup = BeautifulSoup(driver.page_source, "html.parser")
-                try:
-                    job_title = job_soup.find("h1", {"class": "t-24 t-bold inline"}).text.strip()
-                    job_card = job_soup.find('div', class_='job-details-jobs-unified-top-card__primary-description-container')
-                    text_content = job_card.get_text(separator=' ', strip=True)
-                    parts = text_content.split(' · ')
-                    location = parts[0]
-                    time_posted = parts[1]
-                    exact_time = convert_to_datetime(time_posted)
-                    date_format = convert_to_str(exact_time) if exact_time else None
-                    logging.info(f"Title: {job_title}, Location: {location}, Date: {date_format}")
-                except AttributeError as e:
-                    logging.error(f"Lỗi khi lấy thông tin chi tiết công việc: {e}")
-                    continue
-                try:
-                    about_job = job_soup.find('div', id='job-details')
-                    text_about_job = about_job.get_text(separator='\n')
-                    text_about_job_cleaned = re.sub(r'\s+', ' ', text_about_job)
-                    text_about_job_cleaned = re.sub(r'\n+', '\n', text_about_job_cleaned).strip()
-                    logging.info(f"About job: {text_about_job_cleaned[:100]}...")  # Log ngắn gọn mô tả
-                except AttributeError as e:
-                    logging.error(f"Lỗi khi lấy mô tả công việc: {e}")
-                    continue
-                
-                
-                metadata = {
-                    "id_job": job_id,
-                    "job_title": job_title,
-                    "link_post": job_link,
-                    "location": location,
-                    "date": date_format,
-                }
-                
-                doc = Document(metadata=metadata,page_content=text_about_job_cleaned)
-                docs.append(doc)
+            try:
+                about_job = job_soup.find('div', id='job-details')
+                text_about_job = about_job.get_text(separator='\n')
+                text_about_job_cleaned = re.sub(r'\s+', ' ', text_about_job)
+                text_about_job_cleaned = re.sub(r'\n+', '\n', text_about_job_cleaned).strip()
+                logging.info(f"About job: {text_about_job_cleaned[:100]}...")  # Log ngắn gọn mô tả
+            except AttributeError as e:
+                logging.error(f"Lỗi khi lấy mô tả công việc: {e}")
+                continue
+            
+            
+            metadata = {
+                "id_job": job_id,
+                "job_title": job_title,
+                "link_post": job_link,
+                "location": location,
+                "date": date_format,
+            }
+            
+            doc = Document(metadata=metadata,page_content=text_about_job_cleaned)
+            docs.append(doc)
         except AttributeError:
             logging.error("Không tìm thấy ID công việc, bỏ qua công việc này.")
             continue
